5f22fc843d8aeb63fa7754302edc5d838e84a49f
sk <content.type.text.net@gmail.com>
2022-05-06 22:50:34 +0900

    initialize ImageStocker

---
+++ Source/ImageStocker/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ContentTypeTextNet.Pe.Bridge.Plugin;
+using ContentTypeTextNet.Pe.Embedded.Attributes;
+
+[assembly: PluginIdentifiers("ImageStocker", "f3975082-3869-479a-b886-2af522aa305e")]
+[assembly: PluginSupportVersions("0.0.0", "0.0.0", "<https://example.com/update-check.json?")]
+[assembly: PluginAuthors("<author>", PluginLicense.Unknown, "<website>", "<projectsite>", "<email>")]
